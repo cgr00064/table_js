17da78b4bea0a6cdb2babb2cbfcbc83d92ef7de9
Carlos Garvín <cgr00064@red.ujaen.es>
2021-12-13 12:26:00 +0100

    first commit

---
+++ table.js
@@ -0,0 +1,24 @@
+function makeTable(){
+    var table = document.getElementById('tab');
+    var row = document.getElementById('rows').value;
+    var col = document.getElementById('colums').value;
+
+    console.log(row+" "+col);
+
+    for(var rowIndex = 0; rowIndex < row; rowIndex++){
+        var tr = document.createElement('tr');
+
+        for(var colIndex = 0; colIndex < col; colIndex++){
+            var td = document.createElement('td');
+            var text = document.createTextNode("Insert data here "+ colIndex);
+
+            td.appendChild(text);
+            tr.appendChild(td);
+        }
+
+        table.appendChild(tr);
+    }
+}
+
+
+document.getElementById('make').addEventListener("click",makeTable);
